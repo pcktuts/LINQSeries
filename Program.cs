4d50425322acc63aaba9d8896c8003fe3a07ec08
Learn Programming <pcktuts@gmail.com>
2021-10-05 14:40:05 +0530

    02 why use linq

--- Program.cs
+++ Program.cs
@@ -1,45 +1,51 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Xml.Linq;
 
 namespace LINQSeries
 {
+    class Employee
+    {
+        public string Name { get; set; }
+        public int Salary { get; set; }
+        public string Desgination { get; set; }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            // The Three Parts of a LINQ Query:
-            // 1. Data source.
-            int[] numbers = new int[7] { 0, 1, 2, 3, 4, 5, 6 };
-
-            // 2. Query creation.
-            // numQuery is an IEnumerable<int>
-            var numQuery =
-                from num in numbers
-                where (num % 2) == 0
-                select num;
-
-            // 3. Query execution.
-            foreach (int num in numQuery)
+            /*
+             * Why you should use LINQ?
+             */
+            List<Employee> Employees = new List<Employee>()
             {
-                Console.Write("{0,1} \n\n", num);
+                new Employee(){Name="Krishna", Salary= 15000, Desgination = "Developer"},
+                new Employee(){Name="Peter", Salary= 25000, Desgination = "Developer"},
+                new Employee(){Name="Joseph", Salary= 35000, Desgination = "Manager"},
+                new Employee(){Name="Athira", Salary= 11000, Desgination = "Developer"},
+            };
+
+            foreach (var item in Employees)
+            {
+                if(item.Desgination == "Manager")
+                {
+                    Console.WriteLine($"{item.Name}, {item.Desgination}");
+                }
+            }
+            var managers = from e in Employees
+                           where e.Desgination == "Manager"
+                           select e;
+            foreach (var item in managers)
+            {
+                Console.WriteLine($"{item.Name} {item.Desgination}");
             }
 
-            // LINQ query from XML
-            // 1. Data source.
-            XElement contacts = XElement.Load(@"C:\Users\krishnadas\source\repos\LINQSeries\contacts.xml");
-            // 2. Query creation.
-            IEnumerable<XElement> employees = from el in contacts.Elements("Employee")
-                                              where (int)el.Element("EmpId") == 1
-                                              select el;
-
-            // 3. Query execution.
-            foreach (var employee in employees)
+            List<int> nums = new List<int> {22, 45, 66, 88, 90,77 };
+            var evenNums = nums.Where(n => n % 2 == 0);
+            foreach (var item in evenNums)
             {
-                Console.WriteLine(employee);
+                Console.WriteLine(item);
             }
-
         }
     }
 }
